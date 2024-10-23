2a2796a859da34309d05da4b00ac372a5f63e03b
karman011 <karmapolice1431@proton.me>
2024-10-23 13:30:27 +0800

    no starship

--- build_files/fetch-install.sh
+++ build_files/fetch-install.sh
@@ -3,11 +3,11 @@
 set -ouex pipefail
 
 # Starship Shell Prompt
-curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
-tar -xzf /tmp/starship.tar.gz -C /tmp
-install -c -m 0755 /tmp/starship /usr/bin
-# shellcheck disable=SC2016
-echo 'eval "$(starship init bash)"' >> /etc/bashrc
+# curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
+# tar -xzf /tmp/starship.tar.gz -C /tmp
+# install -c -m 0755 /tmp/starship /usr/bin
+# # shellcheck disable=SC2016
+# echo 'eval "$(starship init bash)"' >> /etc/bashrc
 
 # Bash Prexec
 curl -Lo /usr/share/bash-prexec https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh
